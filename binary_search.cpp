0c8372df1071e171cc830630847271d77fab0b53
aw5421 <antek.borkowski123@gmail.com>
2020-09-04 22:05:26 +0200

    implement binary search

--- binary_search.cpp
+++ binary_search.cpp
@@ -6,10 +6,19 @@ int binarySearch(T arr[], int n, T val) {
 	int right = n - 1;
 	while (left <= right) {
 		int mid = left + (right - left) / 2;
+		if (arr[mid] == val)
+			return mid;
+		else if (arr[mid] < val)
+			left = mid + 1;
+		else
+			right = mid - 1;
 	}
+	return -1;
 }
 
 int main(int argc, const char** argv) {
-	std::cout << "hello world" << std::endl;
+	int exampleArr[] = {0, 2, 3, 7, 12, 15};
+	int len = sizeof(exampleArr) / sizeof(exampleArr[0]);
+	std::cout << binarySearch(exampleArr, len, 7) << std::endl;
 	return 0;
 }
