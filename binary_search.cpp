af52007e7ee1144ea9ea6ac1b21630a33cbff5af
aw5421 <antek.borkowski123@gmail.com>
2020-09-04 21:57:09 +0200

    configure basic compiler settings in .vscode

---
+++ binary_search.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+template <class T>
+int binarySearch(T arr[], int n, T val) {
+	int left = 0;
+	int right = n - 1;
+	while (left <= right) {
+		int mid = left + (right - left) / 2;
+	}
+}
+
+int main(int argc, const char** argv) {
+	std::cout << "hello world" << std::endl;
+	return 0;
+}
